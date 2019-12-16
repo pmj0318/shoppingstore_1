81f1d8d1b9cc10b68f6a83a32bd5ce2a8f8af70c
pmj0318 <1422803488@qq.com>
2019-12-16 19:14:17 +0800

    aa

--- src/main/java/controller/FuzzyLookup.java
+++ src/main/java/controller/FuzzyLookup.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import serviceImpl.ProductinfoServiceImpl;
 
 import java.util.List;
+//RestController  /./方法1就是
 //模糊查询
 @Controller
 public class FuzzyLookup {
@@ -21,7 +21,11 @@ public class FuzzyLookup {
    // @ResponseBody
     public String selectLike(@RequestParam String name, Model m){
         System.out.println(name);
-       // System.out.println(psi.selectLike(name));
+        //就是原先就是使用Recontroller状态.就是方法1
+        // System.out.println(psi.selectLike(name));
+        //   return psi.selectLike(name);
+
+       //就是方法2.@Controller 就是自己的,使用model返回
         List<Productinfo> list = psi.selectLike(name);
         m.addAttribute("m",list);
         return "selectLike";
